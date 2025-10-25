4ee115125159db95f9acb7444311657fed938f06
Carlos Villanueva <cs.vp@outlook.com>
2025-10-25 13:10:35 -0600

    style(formatting): improve code formatting using Spotless

--- src/main/java/com/paymentchain/business/billing/BillingApplication.java
+++ src/main/java/com/paymentchain/business/billing/BillingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BillingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BillingApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BillingApplication.class, args);
+  }
 }
