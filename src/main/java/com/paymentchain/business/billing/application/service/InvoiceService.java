cbd405cb11441dca893256dea65bcb092d3c537a
Carlos Villanueva <cs.vp@outlook.com>
2025-10-27 11:31:15 -0600

    feat(service): add Transactional tag to service method

--- src/main/java/com/paymentchain/business/billing/application/service/InvoiceService.java
+++ src/main/java/com/paymentchain/business/billing/application/service/InvoiceService.java
@@ -3,6 +3,7 @@ package com.paymentchain.business.billing.application.service;
 import com.paymentchain.business.billing.application.port.in.CreateInvoicePort;
 import com.paymentchain.business.billing.application.port.out.SaveInvoicePort;
 import com.paymentchain.business.billing.domain.Invoice;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class InvoiceService implements CreateInvoicePort {
 
   private final SaveInvoicePort saveInvoicePort;
 
+  @Transactional
   @Override
   public Invoice createInvoice(Invoice invoice) {
     return saveInvoicePort.saveInvoice(invoice);
